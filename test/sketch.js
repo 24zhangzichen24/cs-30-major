56ef864fca402254dd50a10b744d7bc35b8873fc
24zhangzichen24 <zhangzichen703@gmail.com>
2026-05-14 14:21:51 -0600

    bezier line

--- test/sketch.js
+++ test/sketch.js
@@ -25,22 +25,32 @@ function draw() {
   // bezier(P1X, P1Y, P2X, P2Y, P3X, P3Y, P4X, P4Y)
   // P2 = P1 + (P4 - P1) * Vector(-0.3, 0.8)
   // P3 = P1 + (P4 - P1) * Vector(0.1, 1.4)
+  let x1 = pointX + (mouseX - pointX) * -0.3;
+  let y1 = pointY + (mouseY - pointY) * 0.8;
+  let x2 = pointX + (mouseX - pointX) * 0.1;
+  let y2 = pointY + (mouseY - pointY) * 1.4;
+  
   bezier(pointX, pointY,
-    pointX + (mouseX - pointX) * -0.3, pointY + (mouseY - pointY) * 0.8,
-    pointX + (mouseX - pointX) * 0.1, pointY + (mouseY - pointY) * 1.4,
+    x1, y1,
+    x2, y2,
     mouseX, mouseY
   );
-  let arrowAngle = bezierTangent(pointX, pointY,
-    pointX + (mouseX - pointX) * -0.3, pointY + (mouseY - pointY) * 0.8,
-    pointX + (mouseX - pointX) * 0.1, pointY + (mouseY - pointY) * 1.4,
-    mouseX, mouseY, 0.99
+  let tx = bezierTangent(pointX,
+    x1, 
+    x2,
+    mouseX, 0.99);
+  let ty = bezierTangent(pointY,
+    y1, 
+    y2,
+    mouseY, 0.99);
+  let arrowAngle = atan2(ty, tx
   );
   push();
   translate(mouseX, mouseY);
-  rotate(arrowAngle);
+  rotate(PI);
   image(ArrowImage,-10,-10,20,20);
   pop();
-  
+  console.log(arrowAngle);
  
 }
 
